26edac3158deeaf4973ba1fa98094f2926e38bd2
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-12-31 13:24:52 -0700

    Update input_test.cpp

--- input_test.cpp
+++ input_test.cpp
@@ -1,8 +1,17 @@
 // Unit tests for input.h routines.
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "input.h"
 
-#ifdef _DEBUG
+#ifdef __GNUC__
+#define INVALID_STOD_EXCEPTION "stod"
+#define RANGE_STOL_EXCEPTION "stol"
+#define INVALID_STOL_EXCEPTION "stol"
+#elif _MSC_VER
+#define INVALID_STOD_EXCEPTION "invalid stod argument"
+#define RANGE_STOL_EXCEPTION "stol argument out of range"
+#define INVALID_STOL_EXCEPTION "invalid stol argument"
+#endif
+
 int main() {
 	double d;
 	long l;
@@ -64,20 +73,23 @@ int main() {
 	s = "-1";
 	assert(::isNumber<double>(d, s));
 	assert(d == -1);
+	s = "1";
+	assert(::isNumber(d, s));
+	assert(d == 1);
 
 	s = "-+0"; // invalid, ill-formed
 	try {
 		::isNumber<double>(d, s);
 	}
-	catch (exception e) {
-		assert(e.what() == string("invalid stod argument"));
+	catch (exception const& e) {
+		assert(e.what() == string(INVALID_STOD_EXCEPTION));
 	}
 
 	s = "abc"; // invalid, non-numeric
 	try {
 		::isNumber<double>(d, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Invalid characters"));
 	}
 
@@ -85,7 +97,7 @@ int main() {
 	try {
 		::isNumber<double>(d, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Invalid characters"));
 	}
 
@@ -93,7 +105,7 @@ int main() {
 	try {
 		::isNumber<double>(d, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Invalid characters"));
 	}
 
@@ -101,11 +113,11 @@ int main() {
 	try {
 		::isNumber<double>(d, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Trailing characters"));
 	}
 
-	// Long 
+	// Long
 	s = "0";
 	assert(::isNumber<long>(l, s));
 	assert(l == 0);
@@ -138,36 +150,39 @@ int main() {
 	s = "-1";
 	assert(::isNumber<long>(l, s));
 	assert(l == -1);
+	s = "1";
+	assert(::isNumber(l, s));
+	assert(l == 1);
 
-	s = "2147483648"; // Exceeds max long value.
+	s = "2147483648";
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
-		assert(e.what() == string("stol argument out of range"));
+	catch (exception const& e) {
+		assert(e.what() == string(RANGE_STOL_EXCEPTION));
 	}
 
-	s = "-2147483649"; // Exceed min long value.
+	s = "-2147483649";
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
-		assert(e.what() == string("stol argument out of range"));
+	catch (exception const& e) {
+		assert(e.what() == string(RANGE_STOL_EXCEPTION));
 	}
 
-	s = "-+0"; // Invalid, ill-formed.
+	s = "-+0";
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
-		assert(e.what() == string("invalid stol argument"));
+	catch (exception const& e) {
+		assert(e.what() == string(INVALID_STOL_EXCEPTION));
 	}
 
-	s = "abc"; // Invalid characters.
+	s = "abc";
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Invalid characters"));
 	}
 
@@ -175,27 +190,26 @@ int main() {
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Invalid characters"));
 	}
 
-	s = "1 1"; // Invalid trainling chars.
+	s = "1 1";
 	try {
 		::isNumber<long>(l, s);
 	}
-	catch (exception e) {
+	catch (exception const& e) {
 		assert(e.what() == string("Trailing characters"));
 	}
 
-	// Unsupported type.
 	char c;
 	s = "1";
 	assert(!::isNumber<char>(c, s));
 
 	if (getNumber<double>("Enter a double: ", d, 0., 100.))
-	cout << "double value: " << d << endl;
+		cout << "double value: " << d << endl;
 	if (getNumber<long>("Enter a long: ", l, -100, 100))
-	cout << "long value: " << l << endl;
+		cout << "long value: " << l << endl;
 
+	return 0;
 }
-#endif
